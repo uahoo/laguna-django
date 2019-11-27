2e21d62c57ee237df30a91f58ad0522d31e47527
Yuri <y.kanapelka@gmail.com>
2019-11-27 20:10:41 +0100

    Minor improvements

---
+++ webapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from webapp.models import *
+# Register your models here.
+
+admin.site.register(Users)
+admin.site.register(News)
+admin.site.register(Photos)
+admin.site.register(Gallery)
