c4da7750eef28ba6203db5aa3fa8977d1184decd
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-03-19 17:41:31 +0300

    User model update v 2

--- webapp/User_extender.py
+++ webapp/User_extender.py
@@ -17,3 +17,5 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+Test
