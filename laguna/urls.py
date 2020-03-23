616f98108d12ede46cb4a09b8ca0bef05914e7b0
Yuri Kanapelka <y.kanapelka@gmail.com>
2020-03-23 11:30:32 +0300

    Bug fixes

--- laguna/urls.py
+++ laguna/urls.py
@@ -22,12 +22,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', webapp.views.index, name='index'),
-    path('index', webapp.views.index,name='index'),
+    path('index', webapp.views.index, name='index'),
     path('register', webapp.views.Registration.as_view(), name='register'),
     path('contacts', webapp.views.ContactView.as_view(), name='Contacts'),
     path('about', webapp.views.AboutView.as_view(), name='About'),
-    path('price', webapp.views.PricesView.as_view(),name='Price'),
-    path('login',webapp.views.LoginView.as_view(),name='login'),
-    path('user-page',webapp.views.UserPageView.as_view(),name='user-page')
+    path('price', webapp.views.PricesView.as_view(), name='Price'),
+    path('login', webapp.views.LoginView.as_view(), name='login'),
+    path('user-page', webapp.views.UserPageView.as_view(), name='user-page')
 ]
 # + static(laguna.settings.STATIC_URL, document_root=laguna.settings.STATIC_ROOT)
